04aab895741137c7e2be3d521567384c169c56a8
MickaVincent <mickaelvincent70@outlook.fr>
2017-01-30 10:40:33 +0100

    Petit update

--- main/src/Main/main.java
+++ main/src/Main/main.java
@@ -6,7 +6,7 @@ package Main;
 public class main {
 
     public static void main(String[] args){
-            System.out.println("Slt");
+            System.out.println("Sljitojhhseutt");
     }
 
 }
