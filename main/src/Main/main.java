0fd37cdbcf4b5a7963ef175fdb7f8fcddbecce84
MickaVincent <mickaelvincent70@outlook.fr>
2017-01-30 10:00:18 +0100

    Premier Commit du projet

---
+++ main/src/Main/main.java
@@ -0,0 +1,12 @@
+package Main;
+
+/**
+ * Created by ASUSMickael on 30/01/2017.
+ */
+public class main {
+
+    public static void main(String[] args){
+            System.out.println("Slt");
+    }
+
+}
