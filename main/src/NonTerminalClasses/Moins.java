fc591d0a75c995c988cb0c0d2a1aa27d3e76c432
camille <camille.jeantot@outlook.fr>
2017-02-06 10:28:23 +0100

    Classe entier implémentée

--- main/src/NonTerminalClasses/Moins.java
+++ main/src/NonTerminalClasses/Moins.java
@@ -1,9 +1,15 @@
 package NonTerminalClasses;
 
 import PatronComposite.NonTerminal;
+import TerminalClasses.Entier;
 
 /**
  * Created by Mickael on 06/02/2017.
  */
 public class Moins extends NonTerminal {
+    public Moins(Entier a, Entier b)
+    {
+
+
+    }
 }
