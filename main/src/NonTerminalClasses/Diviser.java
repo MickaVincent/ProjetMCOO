ee1da914d36f82ece76a9982d60fc78fca87e027
Mickael <mickaelvincent70@outlook.fr>
2017-02-06 10:22:36 +0100

    Ajout de plusieures classes pour les symboles non terminaux

---
+++ main/src/NonTerminalClasses/Diviser.java
@@ -0,0 +1,9 @@
+package NonTerminalClasses;
+
+import PatronComposite.NonTerminal;
+
+/**
+ * Created by Mickael on 06/02/2017.
+ */
+public class Diviser extends NonTerminal {
+}
--- main/src/PatronComposite/NonTerminal.java
+++ main/src/PatronComposite/NonTerminal.java
@@ -1,13 +1,18 @@
 package PatronComposite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ASUSMickael on 30/01/2017.
  */
 public abstract class NonTerminal implements Expression {
-    public void ajouter(Expression e){
+    private List<Expression> listExpressions = new ArrayList<>();
 
+    public void ajouter(Expression e){
+        listExpressions.add(e);
     }
     public Expression supprimer(int i){
-        return null;
+        return listExpressions.remove(i);
     }
 }
