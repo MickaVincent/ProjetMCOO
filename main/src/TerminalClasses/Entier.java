bf5cd075622d0088b668ec78f9f74b2c7ec41885
camille <camille.jeantot@outlook.fr>
2017-02-06 10:24:42 +0100

    Classe entier implémentée

--- main/src/TerminalClasses/Entier.java
+++ main/src/TerminalClasses/Entier.java
@@ -6,4 +6,14 @@ import PatronComposite.Terminal;
  * Created by ASUSMickael on 30/01/2017.
  */
 public class Entier extends Terminal {
+    private int m_number;
+    public Entier(int number)
+    {
+        m_number = number;
+    }
+
+    public int getNumber()
+    {
+        return m_number;
+    }
 }
